d5331b756cd82e90dd92b0f689ad069381ec2825
andochiwa <a1066079469@gmail.com>
2021-09-03 00:50:52 +0900

    webflux simulation exceptions to test transaction

--- spring-webflux/src/main/kotlin/com/github/webflux/service/UserService.kt
+++ spring-webflux/src/main/kotlin/com/github/webflux/service/UserService.kt
@@ -3,9 +3,11 @@ package com.github.webflux.service
 import com.github.webflux.dao.UserDao
 import com.github.webflux.entity.User
 import kotlinx.coroutines.flow.Flow
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 /**
  *
@@ -16,6 +18,8 @@ import org.springframework.stereotype.Service
 @Service
 class UserService {
 
+    val log = LoggerFactory.getLogger(UserService::class.java)!!
+
     @Autowired
     private lateinit var userDao: UserDao
 
@@ -23,8 +27,15 @@ class UserService {
         return userDao.findById(id)
     }
 
+    @Transactional
     suspend fun saveUser(user: User): User {
-        return userDao.save(user)
+        log.info("save first: $user")
+        userDao.save(user)
+        val user2 = User(null, "test2", 10)
+        log.info("save second: $user2")
+        val i = 1 / 0
+        return user2
+
     }
 
     fun getUserPagination(current: Int, limit: Int): Flow<User> {
