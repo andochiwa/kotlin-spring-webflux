c75b2733f10506fa797bc6b6d8669a125d2e8a8b
andochiwa <a1066079469@gmail.com>
2021-09-04 03:14:40 +0900

    fixed not automatically imported

--- spring-webflux/src/main/kotlin/com/github/webflux/SpringWebfluxApplication.kt
+++ spring-webflux/src/main/kotlin/com/github/webflux/SpringWebfluxApplication.kt
@@ -2,7 +2,6 @@ package com.github.webflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
 class SpringWebfluxApplication
